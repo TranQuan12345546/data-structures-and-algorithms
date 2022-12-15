5b6d07c69c01a06fdbed5278d72b867bd9b70359
{Quan} <{Trananhquan335@gmail.com}>
2022-12-15 11:40:22 +0700

    btvn

---
+++ src/lesson1/trananhquan/ConcatenationOfArray.java
@@ -0,0 +1,14 @@
+package lesson1.trananhquan;
+
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int a = nums.length;
+        int[] ans = new int[2*a];
+        for (int i = 0; i < a; i++) {
+            ans[i] = nums[i];
+            ans[i + a] = nums[i];
+        }
+        int c = 0;
+        return ans;
+    }
+}
